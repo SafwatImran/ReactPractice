4319076de6dffcc6a0f8f4192f991ea7e1ff07b7
Safwat Imran <safwatimran@gmail.com>
2021-01-29 02:52:51 +0600

    Learned bind

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ const app = props => {
     ], 
     otherState : 'some value'
   });
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     setPersonsState({
       persons :[
       { name : 'Safwat Imran', age : 25},
-      { name : 'Kaisa', age : 23},
+      { name : newName, age : 23},
       { name : 'Fisher', age :26 }
     ], 
     otherState : personState.otherState 
@@ -24,11 +24,11 @@ const app = props => {
   return (
      <div className="App">
       <h1>hi, I'm your first React App</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button onClick={switchNameHandler.bind(this,"Kaiser Soze")}>Switch Name</button>
       <Person name=
       {personState.persons[0].name} age={personState.persons[0].age}/>
       <Person name=
-      {personState.persons[1].name} age={personState.persons[1].age} click ={switchNameHandler}>My Hobbies: Guitar </Person>
+      {personState.persons[1].name} age={personState.persons[1].age} click ={switchNameHandler.bind(this, "JackTheRipper")}>My Hobbies: Guitar </Person>
       <Person name =
       {personState.persons[2].name} age= {personState.persons[2].age}/>
     </div>
