22d84f8654babbb3d70348c733e31855c5beb733
Safwat Imran <safwatimran@gmail.com>
2021-01-29 03:06:01 +0600

    Name Change Handler

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ const app = props => {
     otherState : personState.otherState 
   })
 };  
+  const nameChangedHandler = (event) =>{
+    setPersonsState({
+      persons :[
+      { name : 'Safwat Imran', age : 25},
+      { name : event.target.value, age : 23},
+      { name : 'Fisher', age :26 }
+    ]
+  })
+}
+
   return (
      <div className="App">
       <h1>hi, I'm your first React App</h1>
@@ -28,7 +38,7 @@ const app = props => {
       <Person name=
       {personState.persons[0].name} age={personState.persons[0].age}/>
       <Person name=
-      {personState.persons[1].name} age={personState.persons[1].age} click ={switchNameHandler.bind(this, "JackTheRipper")}>My Hobbies: Guitar </Person>
+      {personState.persons[1].name} age={personState.persons[1].age} click ={switchNameHandler.bind(this, "JackTheRipper")} changed = {nameChangedHandler}>My Hobbies: Guitar </Person>
       <Person name =
       {personState.persons[2].name} age= {personState.persons[2].age}/>
     </div>
